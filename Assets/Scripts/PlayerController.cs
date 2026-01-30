de9c6f3bb144e6c398434d48bc6922454ca3c7f2
Ozan Celik <ozan_celik@ymail.com>
2026-01-30 22:14:37 +0300

    Controls, arena test scene;

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,16 +1,79 @@
+using System.Collections.Generic;
 using UnityEngine;
+
+public enum FacingDirection
+{
+	Up,
+	UpRight,
+	Right,
+	DownRight,
+	Down,
+	DownLeft,
+	Left,
+	UpLeft
+}
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	[Header("Player")]
+	[SerializeField] private bool isPlayer1 = true;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	[Header("Movement")]
+	[SerializeField] private float moveSpeed = 5f;
+	[SerializeField] private bool normalizeDiagonal = true;
+
+	public FacingDirection Facing { get; private set; } = FacingDirection.Down;
+
+	// Optional: last non-zero move direction as a vector (handy for aiming, etc.)
+	public Vector2 LastMoveDir { get; private set; } = Vector2.down;
+
+	private void Update()
+	{
+		UpdateMovement();
+	}
+
+	private void UpdateMovement()
+	{
+		Dictionary<Key, KeyCode> map = isPlayer1 ? Keys.Player1Keys : Keys.Player2Keys;
+
+		int x = 0;
+		int y = 0;
+
+		if (Input.GetKey(map[Key.Left])) x -= 1;
+		if (Input.GetKey(map[Key.Right])) x += 1;
+		if (Input.GetKey(map[Key.Down])) y -= 1;
+		if (Input.GetKey(map[Key.Up])) y += 1;
+
+		Vector2 input = new Vector2(x, y);
+
+		Vector2 move = input;
+		if (normalizeDiagonal && move.sqrMagnitude > 1f)
+			move = move.normalized;
+
+		if (move != Vector2.zero)
+		{
+			transform.position += (Vector3)(move * moveSpeed * Time.deltaTime);
+
+			Facing = GetFacingFromInput(input);
+
+			LastMoveDir = move.normalized;
+		}
+	}
+
+	private static FacingDirection GetFacingFromInput(Vector2 input)
+	{
+		// Assumes input components are in {-1,0,1} from keyboard.
+		int x = (int)Mathf.Sign(input.x);
+		int y = (int)Mathf.Sign(input.y);
+
+		if (x == 0 && y > 0) return FacingDirection.Up;
+		if (x > 0 && y > 0) return FacingDirection.UpRight;
+		if (x > 0 && y == 0) return FacingDirection.Right;
+		if (x > 0 && y < 0) return FacingDirection.DownRight;
+		if (x == 0 && y < 0) return FacingDirection.Down;
+		if (x < 0 && y < 0) return FacingDirection.DownLeft;
+		if (x < 0 && y == 0) return FacingDirection.Left;
+		// x < 0 && y > 0
+		return FacingDirection.UpLeft;
+	}
 }
